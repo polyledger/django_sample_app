39a53ec5e0698a089b199abd389bb5b6b68f126b
Michel Pigassou <mpigassou@zendesk.com>
2018-04-09 19:33:25 -0700

    Updates

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ FROM python:2-onbuild
 COPY start.sh /start.sh
 
 # EXPOSE port 8000 to allow communication to/from server
-EXPOSE 8000
+EXPOSE 80
 
 # CMD specifcies the command to execute to start the server running.
 CMD ["/start.sh"]
--- djangosample/settings.py
+++ djangosample/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4!!ro=bp9po_v^^n^*i!aftl43tv78e#@at0xfdm191m@r3b4j'
+SECRET_KEY = os.environ['SECRET_KEY_BASE']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
