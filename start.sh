9adb8bc9d658dac3eaa3c1027a485ba73d9aa9ae
Michel Pigassou <mpigassou@zendesk.com>
2018-04-14 20:08:08 -0700

    Update port

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 # Start Gunicorn processes
 echo Starting Gunicorn.
 exec gunicorn djangosample.wsgi:application \
-    --bind 0.0.0.0:3000 \
+    --bind 0.0.0.0:$PORT \
     --workers 3 --reload
 #python manage.py runserver 0.0.0.0:3000
